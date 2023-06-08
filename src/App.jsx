4728ffd4bcc3db21f1d43dff9e3fb81af200b16a
gixi <gixipixel@gmail.com>
2023-06-08 01:20:47 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 3rem;
+  color: red;
+`;
+
+function App() {
+  return <Title>Hola React</Title>;
+}
+
+export default App;
